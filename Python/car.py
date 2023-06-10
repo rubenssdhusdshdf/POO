52e875b4ff55e5e39993f3799384e3d60049ce10
Ruben <rubenvegajusto@gmail.com>
2023-06-10 20:58:45 +0100

    declaring constructor method in Python

--- Python/car.py
+++ Python/car.py
@@ -3,3 +3,7 @@ class Car:
     license = str
     driver = str
     passenger = int
+
+def __init__(self, license, driver):
+    self.license = license
+    self.driver = driver
--- Python/main.py
+++ Python/main.py
@@ -1,10 +1,11 @@
 from car import Car
+from account import Account
 
 if __name__ == "__main__":
     print("Hola Mundo")
-    car = Car()
-    car.license = "AMS3232"
-    car.driver = "Andres Herrera"
+    
+    car = Car("AMS234", Account("Andres Herrera", "ANDA876"))
     print(vars(car))
+    print(vars(car.driver))
 
     
