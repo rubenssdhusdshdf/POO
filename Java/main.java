b4c7354641db02f0ca0e69a781319ce8cb8b4657
Ruben <rubenvegajusto@gmail.com>
2023-06-10 20:51:30 +0100

    Declaring a constructor method in JavaScript and Java

--- Java/main.java
+++ Java/main.java
@@ -1,9 +1,9 @@
 class Main { 
     public static void main (String[] args) {
         System.out.println("Hola mundo");
-        Car car = new Car();
-        car.license = "AMQ123";
-        car.driver = "Andres Herrera";
+        Car car = new Car("AMQ123", new Account ("Andres Herrera", "AND123") );
+        // In the line above I am saving this code line car.license = ;
+        // In the line above I am saving this code line car.driver = ;
         car.passenger = 4;
         car.pritnDataCar() //Same than system.out.println("Car License: " car.license), but reutilizing existing code, check also car.Java file for better understandign
     }
